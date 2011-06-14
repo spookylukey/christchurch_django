fd8c5d739e2ed5c07cd64a04446d3dd6bb38f310
Luke Plant <L.Plant.98@cantab.net>
2011-06-14 16:35:49 +0100

    Used STATIC_URL where possible and added jQuery so that the photo changer always works

--- christchurch/settings.py
+++ christchurch/settings.py
@@ -98,6 +98,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.i18n',
     'django.core.context_processors.request',
     'django.core.context_processors.media',
+    'django.core.context_processors.static',
     'cms.context_processors.media',
 )
 
