13ba9609de35898d9662a3feada3616b308271c6
Luke Plant <L.Plant.98@cantab.net>
2011-06-20 03:02:45 +0100

    Added easyfilters to sermons page

--- sermons/views.py
+++ sermons/views.py
@@ -1,8 +1,14 @@
 from django.conf import settings
 from django.shortcuts import render
+from django_easyfilters import FilterSet
 
 from sermons.models import Sermon
 
+class SermonFilterSet(FilterSet):
+    fields = ['speaker']
+
 def index(request):
     sermons = Sermon.objects.filter(published=True)
-    return render(request, "sermons/index.html", {'sermons': sermons})
+    sermonsfilter = SermonFilterSet(sermons, request.GET)
+    return render(request, "sermons/index.html", {'sermonsfilter': sermonsfilter,
+                                                  'sermons': sermonsfilter.qs})
