2765bfb93fd5ecc41386c3fc7570b802bed5f815
Luke Plant <L.Plant.98@cantab.net>
2011-06-25 22:44:43 +0100

    Customised Sermon interface so that when adding you just get an upload field
    Other details are filled out automatically

--- sermons/admin.py
+++ sermons/admin.py
@@ -1,4 +1,7 @@
+from functools import partial
+
 from django.contrib import admin
+from django.forms.models import modelform_factory
 
 from sermons.models import Speaker, Topic, Series, Sermon
 
@@ -21,7 +24,20 @@ class SermonAdmin(admin.ModelAdmin):
     date_hierarchy = 'date_delivered'
     list_per_page = 200
 
+    def get_form(self, request, obj=None, **kwargs):
+        if obj is None:
+            # For add view, we have a simplified form that just has an upload field.
+            return modelform_factory(self.model,
+                                     fields=["sermon"],
+                                     formfield_callback=partial(self.formfield_for_dbfield, request=request),)
+        else:
+            return super(SermonAdmin, self).get_form(request, obj=obj, **kwargs)
 
+    def save_model(self, request, obj, form, change):
+        if obj.pk is None:
+            from sermons.tags import set_attrs_from_filename
+            set_attrs_from_filename(obj)
+        obj.save()
 
 admin.site.register(Speaker, SpeakerAdmin)
 admin.site.register(Topic, TopicAdmin)
