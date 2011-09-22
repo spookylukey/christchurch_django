798bdb99a318f29320145d3f1cee8f3289093cb3
Luke Plant <L.Plant.98@cantab.net>
2011-09-22 01:02:29 +0100

    Fixed update_home_group_lists to create list email if necessary.

--- contacts/lists.py
+++ contacts/lists.py
@@ -11,8 +11,15 @@ def update_home_group_lists(*args, **kwargs):
         # This happens during development
         return
 
+    webfaction_email_list = [d['email_address'] for d in s.list_emails()]
+
     for hg in HomeGroup.objects.exclude(group_email=''):
         if hg.group_email != '':
             email_list = list(set([c.email for c in hg.contact_set.exclude(email='')]))
-            s.update_email(hg.group_email, ', '.join(email_list))
-
+            email_list_s = ', '.join(email_list)
+            if hg.group_email in webfaction_email_list:
+                # Update:
+                s.update_email(hg.group_email, email_list_s)
+            else:
+                # Create new
+                s.create_email(hg.group_email, email_list_s)
