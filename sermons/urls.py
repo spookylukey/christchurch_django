6f8aea8b77cee894bef2f01b741856098e3894b6
Luke Plant <L.Plant.98@cantab.net>
2015-05-25 10:25:19 +0100

    More accurate URL matcher for sermons

--- sermons/urls.py
+++ sermons/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('sermons.views',
-                       url(r'^read/(.*)/', 'read', name='sermon_read'),
+                       url(r'^read/(\d*)/', 'read', name='sermon_read'),
                        url(r'^$', 'index')
                        )
